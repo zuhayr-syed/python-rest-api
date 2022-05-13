494ed00f04d5e1bac4e74f7557897993ae733f30
Zuhayr Syed <zuhxyr.s6@hotmail.com>
2022-05-13 00:28:17 -0400

    added url re-routing to long urls

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const Url = require('../models/Url');
+
+// @route GET /:code
+// @desc Redirect to the long/original URL
+router.get('/:code', async(req, res) =>{
+    try {
+        const url = await Url.findOne({ urlCode: req.params.code });
+
+        if(url) {
+            return res.redirect(url.longUrl);
+        }
+        else{
+            return res.status(404).json('No URL found');
+        }
+    } catch(err) {
+        console.log(err);
+        res.status(500).json('Server error');
+    }
+});
+
+module.exports = router;
